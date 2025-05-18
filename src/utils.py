b17cb626b230e480d222329e7c1627b94dee326e
BenGJ10 <bengj1015@gmail.com>
2025-05-18 20:53:10 +0530

    Changes made

--- src/utils.py
+++ src/utils.py
@@ -34,10 +34,8 @@ def evaluate_model(X_train, Y_train, X_test, Y_test, models, params):
             model.set_params(**gs.best_params_)
             model.fit(X_train, Y_train)
 
-            Y_train_pred = model.predict(X_train)
             Y_test_pred = model.predict(X_test)
 
-            train_model_score = r2_score(Y_train, Y_train_pred)
             test_model_score = r2_score(Y_test, Y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
